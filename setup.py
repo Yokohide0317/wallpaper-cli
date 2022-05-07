e5c6436b3f0a316e08ac0deded7c5545ddc2893c
Yokohide0317 <yokohide0317@gmail.com>
2022-05-07 22:07:43 +0900

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
 
 setup(
-        install_requires=[],#'sys' 
+        name="w-cli",
+        version="0.0.1",
+        description="Wallpaper manager for macOS",
+        author="Yokohide0317",
+        install_requires=[],
         entry_points={
             "console_scripts":[
                 "w-cli = wallpaper:main"
