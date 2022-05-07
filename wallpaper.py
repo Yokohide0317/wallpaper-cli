4b668573f32f9e175cc0ed5c2d41313fd74f83c0
Yokohide0317 <yokohide0317@gmail.com>
2022-05-07 10:17:00 +0900

    ベースの完成

--- wallpaper.py
+++ wallpaper.py
@@ -1,32 +1,64 @@
 #import subprocess
 import os
 import argparse
+import shutil
+import pathlib
+import glob
 
+# ~/.wallpaper
+save_dir = pathlib.Path(os.environ['HOME'], ".wallpaper")
 
+# 画像を~/.wallpaper下へ
+def add_wallpaper(args):
 
-def main(_fs_args):
-
-    if _fs_args.command == "add":
-        parser = argparse.ArgumentParser()
-        parser.add_argument("img_path", nargs="*")
-        parser.add_argument("-n", "--name", nargs="*")
-
-
-    elif _fs_args.command == "to":
-        parser = argparse.ArgumentParser()
-        parser.add_argument("img_name")
-        args = parser.parse_args()
-        change_wallpaper(args.img_name)
+    # 変更前と変更先のPathを確認して、取得
+    def _set_path(_froml, _tol):
+        if _tol != None:
+            assert len(_froml) == len(_tol), "--nameの数が違います"
+            
+            new_froml = []
+            new_tol = []
+            for f, t in zip(_froml, _tol):
+                assert pathlib.Path(f).exists(), f"{f}は存在しません。"
+                new_froml.append(pathlib.Path(f))
+                new_tol.append(save_dir / t)
+        else:
+            new_froml = []
+            new_tol = []
+            for f in _froml:
+                assert pathlib.Path(f).exists(), f"{f}は存在しません。"
+                new_froml.append(pathlib.Path(f))
+                new_tol.append(save_dir)
+        return new_froml, new_tol
+    
+    # ~/.wallpaperが無ければ作成
+    if not os.path.isdir(save_dir):
+        os.mkdir(save_dir)
+    
+    # 呼び出し
+    from_paths, to_paths = _set_path(args.img_path, args.name)
+    for f, t in zip(from_paths, to_paths):
+        shutil.move(f, t)
+        print(f"{f} -> {t.name}")
     return
 
-def change_wallpaper(_img_path):
-    cmd_img_path = '\"' + str(_img_path) + '\"'
+def change_wallpaper(args):
+    img_path = save_dir / pathlib.Path(args.img_name)
+    assert img_path.exists(), f"存在しません: {str(img_path)}"
+    cmd_img_path = '\"' + str(img_path) + '\"'
     cmd = f"osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file {cmd_img_path}'"
 
     print(cmd)
     os.system(cmd)
     return
 
+def list_wallpaper(args):
+    files = [str(x.name) for x in save_dir.iterdir() if x.is_file()]
+    print(files)
+    return
+
+def command_help(args):
+    print(parser.parse_args([args.command, '--help']))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="")
@@ -34,14 +66,18 @@ if __name__ == "__main__":
 
     # addコマンド
     parser_add = subparsers.add_parser('add', help='see `add -h`')
-    parser.add_argument("img_path", nargs="*")
-    parser.add_argument("-n", "--name", nargs="*")
+    parser_add.add_argument("img_path", nargs="*")
+    parser_add.add_argument("-n", "--name", nargs="*")
     parser_add.set_defaults(handler=add_wallpaper)
 
     # toコマンド
     parser_to = subparsers.add_parser('to', help='see `to -h`')
-    parser.add_argument("img_name")
-    parser_add.set_defaults(handler=change_wallpaper)
+    parser_to.add_argument("img_name")
+    parser_to.set_defaults(handler=change_wallpaper)
+
+    # listコマンド
+    parser_list = subparsers.add_parser('list', help='see `to -h`')
+    parser_list.set_defaults(handler=list_wallpaper)
 
     # help
     parser_help = subparsers.add_parser('help', help='see `help -h`')
